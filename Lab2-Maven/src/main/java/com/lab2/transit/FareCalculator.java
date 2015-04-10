d834167a277723ccc9b67a04fdbec31ec3795e5e
zeldanerd24 <kevin.rothenberger@gmail.com>
2015-04-09 20:17:16 -0400

    Added main method to FareCalculator

--- Lab2-Maven/src/main/java/com/lab2/transit/FareCalculator.java
+++ Lab2-Maven/src/main/java/com/lab2/transit/FareCalculator.java
@@ -1,6 +1,29 @@
 package com.lab2.transit;
 
+import java.util.Scanner;
+
 public class FareCalculator {
+	
+	private static Scanner scanner;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Fare Calculator Program!");
+		
+		while(true) {
+			scanner = new Scanner(System.in);
+			System.out.print("Please enter the age (-1 to quit): ");
+			int age = scanner.nextInt();
+			if(age == -1) {
+				break;
+			}
+			System.out.print("Please enter the time: ");
+			String time = scanner.next();
+			System.out.print("Is today a holiday (True or False)?");
+			boolean isHoliday = scanner.nextBoolean();
+			System.out.println("Your fare is: " + calculateFare(age, time, isHoliday));
+		}
+
+	}
 	
 	/**
 	 * 
